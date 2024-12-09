a89e8cbcd98798e77577e22cb214e27eec9adccd
BeataNowakowska <the.beata.nowakowska@gmail.com>
2024-12-09 21:13:13 +0100

    Update CreateTableForMissions.sql

--- CreateTableForMissions.sql
+++ CreateTableForMissions.sql
@@ -24,19 +24,12 @@ CREATE TABLE OdbyteMisje (
     FOREIGN KEY (id_misji) REFERENCES Misje(id_misji)
 );
 
-CREATE TABLE RodzajePrzedmiotow (
-    id_przedmiotu INT PRIMARY KEY,
-    nazwa VARCHAR(100) NOT NULL,
-    opis TEXT
-);
-
 CREATE TABLE Ekwipunek (
     id_ekwipunku INT PRIMARY KEY,
     id_bohatera INT,
-    id_przedmiotu INT,
+    przedmiot VARCHAR(100) NOT NULL,
     ilosc INT DEFAULT 1,
-    FOREIGN KEY (id_bohatera) REFERENCES Bohaterowie(id_bohatera),
-    FOREIGN KEY (id_przedmiotu) REFERENCES RodzajePrzedmiotow(id_przedmiotu)
+    FOREIGN KEY (id_bohatera) REFERENCES Bohaterowie(id_bohatera)
 );
 
 -- Wstawianie danych do tabeli Bohaterowie
@@ -54,23 +47,21 @@ INSERT INTO Misje (id_misji, nazwa, opis, nagroda) VALUES
 (3, 'Eskorta kupca', 'Dostarcz kupca do sąsiedniego miasta.', '20 sztuk złota, Mikstura leczenia'),
 (4, 'Odnalezienie starożytnej relikwii', 'Znajdź zaginiony artefakt w ruinach.', '100 sztuk złota, Pierścień mocy');
 
--- Wstawianie danych do tabeli RodzajePrzedmiotow
-INSERT INTO RodzajePrzedmiotow (id_przedmiotu, nazwa, opis) VALUES
-(1, 'Miecz', 'Ostry miecz wykonany z wytrzymałej stali'),
-(2, 'Tarcza', 'Solidna tarcza z drewna i metalu'),
-(3, 'Łuk', 'Lekki łuk idealny dla łucznika'),
-(4, 'Kostur', 'Magiczny kostur wspomagający rzucanie zaklęć'),
-(5, 'Księga zaklęć', 'Zawiera wiele potężnych zaklęć'),
-(6, 'Mikstura leczenia', 'Leczy rany i przywraca zdrowie');
-
 -- Wstawianie danych do tabeli Ekwipunek
-INSERT INTO Ekwipunek (id_ekwipunku, id_bohatera, id_przedmiotu, ilosc) VALUES
-(1, 1, 1, 1), -- Aragorn ma miecz
-(2, 1, 2, 1), -- Aragorn ma tarczę
-(3, 2, 3, 1), -- Legolas ma łuk
-(4, 2, 6, 2), -- Legolas ma 2 mikstury leczenia
-(5, 3, 4, 1), -- Gandalf ma kostur
-(6, 3, 5, 1), -- Gandalf ma księgę zaklęć
-(7, 4, 1, 1), -- Boromir ma miecz
-(8, 4, 2, 1); -- Boromir ma tarczę
+INSERT INTO Ekwipunek (id_ekwipunku, id_bohatera, przedmiot, ilosc) VALUES
+(1, 1, 'Miecz', 1),
+(2, 1, 'Tarcza', 1),
+(3, 2, 'Łuk', 1),
+(4, 2, 'Strzały', 20),
+(5, 3, 'Kostur', 1),
+(6, 3, 'Księga zaklęć', 1),
+(7, 4, 'Miecz', 1),
+(8, 4, 'Tarcza', 1);
 
+-- Wstawianie danych do tabeli OdbyteMisje
+INSERT INTO OdbyteMisje (id_odbytej_misji, id_bohatera, id_misji, data_odbycia, wynik) VALUES
+(1, 1, 1, '2024-12-01', 'Sukces'),
+(2, 2, 3, '2024-12-02', 'Sukces'),
+(3, 3, 2, '2024-12-03', 'Porażka'),
+(4, 4, 4, '2024-12-05', 'Sukces'),
+(5, 5, 1, '2024-12-06', 'Sukces');
