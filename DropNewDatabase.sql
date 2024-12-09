d15084105d1230bbc0dddd4e78497e0d9937cd90
BeataNowakowska <the.beata.nowakowska@gmail.com>
2024-12-09 21:01:19 +0100

    Create DropNewDatabase.sql

---
+++ DropNewDatabase.sql
@@ -0,0 +1,6 @@
+-- Usuwanie tabel
+DROP TABLE IF EXISTS Ekwipunek;
+DROP TABLE IF EXISTS RodzajePrzedmiotow;
+DROP TABLE IF EXISTS OdbyteMisje;
+DROP TABLE IF EXISTS Misje;
+DROP TABLE IF EXISTS Bohaterowie;
