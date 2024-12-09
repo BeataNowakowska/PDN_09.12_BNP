64bf312a702fa6f160f0a36bf9c539d00558cfc4
BeataBNP <beata.nowakowska@bnpparibas.pl>
2024-12-09 21:33:41 +0100

    Update WeAdAss.sql

--- WeAdAss.sql
+++ WeAdAss.sql
@@ -1,2 +1,4 @@
 insert into Bohaterowie values (6,"Beata","Mag","Uczenie programowania nietechnicznych",1000)
 insert into Ekwipunek values (9,6,"Replit",1)
+insert into Bohaterowie values (7,"BNPBeata","Magik","Uczenie programowania nietechnicznych",1000)
+insert into Ekwipunek values (9,6,"Replittt",1)
