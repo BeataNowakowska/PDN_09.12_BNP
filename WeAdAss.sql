95de476c9edb8c1fca80908479dbcad723a558af
BeataNowakowska <the.beata.nowakowska@gmail.com>
2024-12-10 15:08:06 +0100

    Update WeAdAss.sql

--- WeAdAss.sql
+++ WeAdAss.sql
@@ -1,3 +1,6 @@
 insert into Bohaterowie values (6,"Beata","Mag","Uczenie programowania nietechnicznych",1000)
 insert into Ekwipunek values (9,6,"Replit",1)
 
+insert into Bohaterowie values (10,"Pyzata","Czarodziejka","Zaklecia",1010)
+insert into Ekwipunek values (10,10,"Git",1)
+
