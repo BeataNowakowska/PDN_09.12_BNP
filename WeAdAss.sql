566f6c43009c7f78c695a8b9200dd18b43a90ba5
MartaBere0 <martus0709@gmail.com>
2024-12-10 15:08:18 +0100

    Update WeAdAss.sql

--- WeAdAss.sql
+++ WeAdAss.sql
@@ -2,3 +2,5 @@ insert into Bohaterowie values (6,"Beata","Mag","Uczenie programowania nietechni
 insert into Ekwipunek values (9,6,"Replit",1)
 insert into Bohaterowie values (7,"BNPBeata","Magik","Uczenie programowania nietechnicznych",1000)
 insert into Ekwipunek values (9,6,"Replittt",1)
+insert into Bohaterowie values (6,"Marta","Druid","Cierpliwość",10000)
+insert into Ekwipunek values (9,6,"Laptop",1)
