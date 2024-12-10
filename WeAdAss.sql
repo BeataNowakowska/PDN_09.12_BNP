5548d5e524dbd26282e1386eb5a05e49043062fb
Mirek1793 <miroslaw.bugaj0@gmail.com>
2024-12-10 15:09:06 +0100

    Update WeAdAss.sql

--- WeAdAss.sql
+++ WeAdAss.sql
@@ -1,4 +1,2 @@
-insert into Bohaterowie values (6,"Beata","Mag","Uczenie programowania nietechnicznych",1000)
-insert into Ekwipunek values (9,6,"Replit",1)
-insert into Bohaterowie values (7,"BNPBeata","Magik","Uczenie programowania nietechnicznych",1000)
+insert into Bohaterowie values (7,"BNPBeata","Miro","Uczenie programowania nietechnicznych",1000)
 insert into Ekwipunek values (9,6,"Replittt",1)
